8d1dd698d8f66d8f98d7853d7eb2a16efda6bc67
PeterChou2 <peterisawesome05@gmail.com>
2020-10-18 04:32:25 -0400

    heroku

--- lib/server/config/gameConfig.d.ts
+++
@@ -1,9 +0,0 @@
-import { StartLevel } from '../scene/scene1';
-export declare const config: {
-    type: number;
-    parent: string;
-    width: number;
-    height: number;
-    scene: typeof StartLevel;
-    callbacks: any;
-};
