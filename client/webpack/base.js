1f4c97fc8b090a6c36abfeb611135350b3872a92
PeterChou2 <peterisawesome05@gmail.com>
2021-01-16 23:08:57 -0500

    path

--- client/webpack/base.js
+++ client/webpack/base.js
@@ -2,6 +2,8 @@ const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+console.log('path resolved: ', path.resolve(__dirname, "../"));
+
 module.exports = {
   mode: "development",
   devtool: "eval-source-map",
